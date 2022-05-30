5cc5613e92d5a84b43389834e16212ccad93638b
Michał Głos <kornik90@poczta.fm>
2022-05-30 10:41:47 +0200

    background changes and formating sites

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ const age = 32;
 
 console.log(firstName);
 console.log(age)
-console.log(`Mam na imię ${firstName} i mam ${age} lata.`);
+console.log(`Cześć! Mam na imię ${firstName} i mam ${age} lata.`);
