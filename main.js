e5d0fae24e49f96a15b1bd693d6fe806181efe8a
Michał Głos <kornik90@poczta.fm>
2022-05-31 10:53:14 +0200

    Menu and article styles, added BEM

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ const age = 32;
 console.log(firstName);
 console.log(age)
 console.log(`Cześć! Mam na imię ${firstName} i mam ${age} lata.`);
+
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `Cześć! Mam na imię ${firstName} i mam ${age} lata.`
