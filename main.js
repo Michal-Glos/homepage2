b88f6c0aa9ccfe72ef7c1766b8a3713bcdaa4445
Michał Głos <kornik90@poczta.fm>
2022-06-07 11:42:41 +0200

    Added BEM

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ console.log(age)
 console.log(`Cześć! Mam na imię ${firstName} i mam ${age} lata.`);
 
 
-const heading = document.querySelector('.main-heading--js');
+const heading = document.querySelector('.main__heading--js');
 
 heading.innerHTML = `Cześć! Mam na imię ${firstName} i mam ${age} lata.`
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+
+emptyParagraph.innerHTML = 'Nawet uzupełniłem pracę domową Javascriptem!';
